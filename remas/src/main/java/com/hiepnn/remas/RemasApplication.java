bbe53caf5d37da431589cdd65b7a26e51255970a
hiepdev582 <hiepdev582@gmail.com>
2026-05-28 16:50:29 +0700

    chore(base): initialize be project

---
+++ remas/src/main/java/com/hiepnn/remas/RemasApplication.java
@@ -0,0 +1,13 @@
+package com.hiepnn.remas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemasApplication.class, args);
+	}
+
+}
